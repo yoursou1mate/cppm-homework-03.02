90beed51cd83055e3bd312f068989f9752361efe
a1ex.solovyov <a1ex.solovyov10@gmail.com>
2023-06-19 20:46:33 +0300

    Second commit

--- cppm-homework-03.02/main.cpp
+++ cppm-homework-03.02/main.cpp
@@ -6,9 +6,65 @@
 //
 
 #include <iostream>
+#include <string>
+
+class Counter
+{
+   public:
+    int num = 1;
+    char a;
+    
+    void counter ()
+    {
+       for ( ; ; )
+       {
+           std::cout << '\n' << "Enter ('+', '-', '=' or 'x'): ";
+           std::cin >>  a;
+           if (a == '+')
+           {
+               num++;
+           }
+           else if (a == '-')
+           {
+               num--;
+           }
+           else if (a == '=')
+           {
+               std::cout << num;
+           }
+           else if (a == 'x')
+           {
+               std::cout << "Good bye!" << std::endl;
+               break;
+           }
+       }
+    }
+    
+    
+    
+};
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+   
+    Counter  counter;
+    std::string answer;
+    std::string no;
+    std::cout << "Do  you want to enter a number for the counter? Enter yes or no: ";
+    std::cin >> answer;
+    
+if (answer == "yes")
+    {
+        std::cout << '\n';
+        std::cout << "Enter a number for the counter: " ;
+        std::cin >> counter.num;
+    }
+
+    counter.counter();
+    
+    
+    
+    
+    
+    
     return 0;
 }
