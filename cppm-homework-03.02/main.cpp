8c13524c127ecd08840c8052271572c27e737445
a1ex.solovyov <a1ex.solovyov10@gmail.com>
2023-10-19 19:15:21 +0300

    03.02_Fixed

--- cppm-homework-03.02/main.cpp
+++ cppm-homework-03.02/main.cpp
@@ -10,45 +10,52 @@
 
 class Counter
 {
-   public:
-    int num = 1;
-    char a;
+private:
+    int num;
     
-    void counter ()
+    
+public:
+    
+    int set_num (int num)
     {
-       for ( ; ; )
-       {
-           std::cout << '\n' << "Enter ('+', '-', '=' or 'x'): ";
-           std::cin >>  a;
-           if (a == '+')
-           {
-               num++;
-           }
-           else if (a == '-')
-           {
-               num--;
-           }
-           else if (a == '=')
-           {
-               std::cout << num;
-           }
-           else if (a == 'x')
-           {
-               std::cout << "Good bye!" << std::endl;
-               break;
-           }
-       }
+        std::cin >> num;
+        this->num = num;
+        return num;
     }
     
+    Counter (int num) {
+        this->num = num;
+    }
     
+    Counter ()
+    {
+        num = 1;
+    }
     
+    void counter_plus (int num)
+    {
+       this->num++ == num;
+    }
+    
+    void counter_minus (int num)
+    {
+        this->num-- == num;
+    }
+    
+    void counter_value ()
+    {
+        
+        std::cout << num;
+    }
 };
 
 int main(int argc, const char * argv[]) {
-   
-    Counter  counter;
+    
+    Counter counter;
     std::string answer;
     std::string no;
+    char a;
+    
     std::cout << "Do  you want to enter a number for the counter? Enter yes or no: ";
     std::cin >> answer;
     
@@ -56,15 +63,31 @@ if (answer == "yes")
     {
         std::cout << '\n';
         std::cout << "Enter a number for the counter: " ;
-        std::cin >> counter.num;
+        counter.set_num(0);
     }
-
-    counter.counter();
     
-    
-    
-    
-    
+    for ( ; ; )
+    {
+        std::cout << '\n' << "Enter ('+', '-', '=' or 'x'): ";
+        std::cin >> a;
+        if (a == '+')
+        {
+            counter.counter_plus(0);
+        }
+        else if (a == '-')
+        {
+            counter.counter_minus(0);
+        }
+        else if (a == '=')
+        {
+            counter.counter_value();
+        }
+        else if (a == 'x')
+        {
+            std::cout << "Good bye!" << std::endl;
+            break;
+        }
+    }
     
     return 0;
 }
