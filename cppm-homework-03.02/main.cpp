e93cf17a387d30ceab0cbc87514da874707ee3f5
a1ex.solovyov <a1ex.solovyov10@gmail.com>
2023-10-25 19:01:34 +0300

    03.02_Fixed

--- cppm-homework-03.02/main.cpp
+++ cppm-homework-03.02/main.cpp
@@ -16,12 +16,13 @@ private:
     
 public:
     
-    int set_num (int num)
-    {
-        std::cin >> num;
-        this->num = num;
-        return num;
-    }
+   
+//    int set_num (int num)
+//    {
+//        std::cin >> num;
+//        this->num = num;
+//        return num;
+//    }
     
     Counter (int num) {
         this->num = num;
@@ -34,27 +35,28 @@ public:
     
     void counter_plus (int num)
     {
-       this->num++ == num;
+       this->num++;
     }
     
     void counter_minus (int num)
     {
-        this->num-- == num;
+        this->num--;
     }
     
-    void counter_value ()
+    int counter_value ()
     {
-        
-        std::cout << num;
+        return num;
     }
 };
 
 int main(int argc, const char * argv[]) {
     
-    Counter counter;
+   
     std::string answer;
     std::string no;
     char a;
+    Counter counter;
+    int i;
     
     std::cout << "Do  you want to enter a number for the counter? Enter yes or no: ";
     std::cin >> answer;
@@ -63,31 +65,83 @@ if (answer == "yes")
     {
         std::cout << '\n';
         std::cout << "Enter a number for the counter: " ;
-        counter.set_num(0);
-    }
-    
-    for ( ; ; )
+        std::cin >> i;
+        Counter counter (i);
+        for ( ; ; )
+        {
+            std::cout << '\n' << "Enter ('+', '-', '=' or 'x'): ";
+            std::cin >> a;
+            if (a == '+')
+            {
+                counter.counter_plus(i);
+            }
+            else if (a == '-')
+            {
+                counter.counter_minus(i);
+            }
+            else if (a == '=')
+            {
+                std::cout << counter.counter_value();
+            }
+            else if (a == 'x')
+            {
+                std::cout << "Good bye!" << std::endl;
+                break;
+            }
+        }
+        
+    } else
     {
-        std::cout << '\n' << "Enter ('+', '-', '=' or 'x'): ";
-        std::cin >> a;
-        if (a == '+')
+        for ( ; ; )
         {
-            counter.counter_plus(0);
+            std::cout << '\n' << "Enter ('+', '-', '=' or 'x'): ";
+            std::cin >> a;
+            if (a == '+')
+            {
+                counter.counter_plus(0);
+            }
+            else if (a == '-')
+            {
+                counter.counter_minus(0);
+            }
+            else if (a == '=')
+            {
+                std::cout << counter.counter_value();
+            }
+            else if (a == 'x')
+            {
+                std::cout << "Good bye!" << std::endl;
+                break;
+            }
         }
-        else if (a == '-')
-        {
-            counter.counter_minus(0);
-        }
-        else if (a == '=')
-        {
-            counter.counter_value();
-        }
-        else if (a == 'x')
-        {
-            std::cout << "Good bye!" << std::endl;
-            break;
-        }
+        
     }
     
+
+   
+    
+//    for ( ; ; )
+//    {
+//        std::cout << '\n' << "Enter ('+', '-', '=' or 'x'): ";
+//        std::cin >> a;
+//        if (a == '+')
+//        {
+//            counter.counter_plus(i);
+//        }
+//        else if (a == '-')
+//        {
+//            counter.counter_minus(i);
+//        }
+//        else if (a == '=')
+//        {
+//            std::cout << counter.counter_value();
+//        }
+//        else if (a == 'x')
+//        {
+//            std::cout << "Good bye!" << std::endl;
+//            break;
+//        }
+//    }
+//
     return 0;
 }
